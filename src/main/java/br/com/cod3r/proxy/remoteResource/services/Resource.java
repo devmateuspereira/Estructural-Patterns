f5d815682446b168a051ddf4c1546a3c81083b58
Mateus Pereira Jorge <mateus.p.jorge@gmail.com>
2023-08-24 09:53:55 -0300

    created second example of design pattern proxy

--- src/main/java/br/com/cod3r/proxy/remoteResource/services/Resource.java
+++ src/main/java/br/com/cod3r/proxy/remoteResource/services/Resource.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.proxy.remoteResource.services;
 
 public interface Resource {
-	String getData();
+
+    String getData();
+
 }
--- src/main/java/br/com/cod3r/proxy/remoteResource/services/ResourceProxy.java
+++ src/main/java/br/com/cod3r/proxy/remoteResource/services/ResourceProxy.java
@@ -1,24 +1,29 @@
 package br.com.cod3r.proxy.remoteResource.services;
 
 public class ResourceProxy implements Resource {
-	private Resource realResource;
-	private String validToken = "v@lid!";
-	private String tokenProvided;
-	
-	public ResourceProxy(String tokenProvided) {
-		this.tokenProvided = tokenProvided;
-	}
 
-	@Override
-	public String getData() {
-		if(!tokenProvided.equals(validToken)) return "Invalid Token";
-		if(realResource == null) {
-			try {
-				realResource = new ResourceImpl();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		return realResource.getData();
-	}
+    private Resource realResource;
+
+    private String validToken = "v@lid!";
+
+    private String tokenProvided;
+
+    public ResourceProxy(String tokenProvided) {
+        this.tokenProvided = tokenProvided;
+    }
+
+    @Override
+    public String getData() {
+        if (!tokenProvided.equals(validToken))
+            return "Invalid Token";
+        if (realResource == null) {
+            try {
+                realResource = new ResourceImpl();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return realResource.getData();
+    }
+
 }
